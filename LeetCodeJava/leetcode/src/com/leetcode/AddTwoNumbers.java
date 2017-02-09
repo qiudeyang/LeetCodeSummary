735f1cc8bd573a1f8d1f650deb2f5e369d580ed7
qiudeyang <969387319@qq.com>
2017-02-09 16:06:41 +0800

    update

--- LeetCodeJava/leetcode/src/com/leetcode/AddTwoNumbers.java
+++ LeetCodeJava/leetcode/src/com/leetcode/AddTwoNumbers.java
@@ -2,41 +2,123 @@ package com.leetcode;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
+import java.util.Stack;
 
 /**
  * Created by qiudeyang on 08/10/16.
  */
 class Solution2 {
-    public LinkedList addTwoNumbers(LinkedList l1, LinkedList l2) {
-        int a = 0;
-        int b = 0;
-        for (int i = 0; i < l1.size(); i++) {
-            a += (int)l1.get(i) * (int)Math.pow(10,i);
+    //方法1使用了O(n)空间消耗
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        if (l1 == null && l2 == null){
+            return null;
         }
-        for (int i = 0; i < l2.size(); i++) {
-            b += (int)l2.get(i) * (int)Math.pow(10,i);
+        List<Integer> list = new LinkedList<Integer>();
+        int flag = 0;
+        while (l1 != null && l2 != null){
+            int result = l1.val + l2.val +flag;
+            list.add(result%10);
+            if (result >= 10){
+                flag = 1;
+            }else{
+                flag = 0;
+            }
+            l1 = l1.next;
+            l2 = l2.next;
         }
-        int c = a+b;
-        System.out.println(c);
-        LinkedList l3 = new LinkedList();
-        char[] array = (c+"").toCharArray();
-        for (int i = array.length-1; i >= 0; i--) {
-            l3.add(array[i]);
+        while (l1 != null){
+            int result = l1.val+flag;
+            list.add(result%10);
+            if (result >= 10){
+                flag = 1;
+            }else{
+                flag = 0;
+            }
+            l1 = l1.next;
         }
-        return l3;
+        while (l2 != null){
+            int result = l2.val +flag;
+            list.add(result%10);
+            if (result >= 10){
+                flag = 1;
+            }else{
+                flag = 0;
+            }
+            l2 = l2.next;
+        }
+        ListNode result = new ListNode(list.get(0));
+        ListNode temp = result;
+        for (int i = 1; i < list.size(); i++) {
+            temp.next = new ListNode(list.get(i));
+            temp = temp.next;
+        }
+        if (flag == 1){
+            temp.next = new ListNode(1);
+        }
+        return result;
+    }
+    public ListNode addTwoNumbers1(ListNode l1, ListNode l2) {
+        if (l1 == null && l2 == null){
+            return null;
+        }
+        int flag = 0;
+        ListNode result = new ListNode(0);
+        ListNode temp = result;
+        while (l1 != null && l2 != null){
+            int temp1 = l1.val + l2.val +flag;
+            temp.next = new ListNode(temp1%10);
+            temp = temp.next;
+            if (temp1 >= 10){
+                flag = 1;
+            }else{
+                flag = 0;
+            }
+            l1 = l1.next;
+            l2 = l2.next;
+        }
+        while (l1 != null){
+            int temp1 = l1.val+flag;
+            temp.next = new ListNode(temp1%10);
+            temp = temp.next;
+            if (temp1 >= 10){
+                flag = 1;
+            }else{
+                flag = 0;
+            }
+            l1 = l1.next;
+        }
+        while (l2 != null){
+            int temp1 = l2.val +flag;
+            temp.next = new ListNode(temp1%10);
+            temp = temp.next;
+            if (temp1 >= 10){
+                flag = 1;
+            }else{
+                flag = 0;
+            }
+            l2 = l2.next;
+        }
+        if (flag == 1){
+            temp.next = new ListNode(1);
+        }
+        return result.next;
     }
 }
+
 public class AddTwoNumbers {
     public static void main(String[] args) {
         Solution2 solution2 = new Solution2();
-        LinkedList list1 = new LinkedList();
-        LinkedList list2 = new LinkedList();
-        list1.add(2);
-        list1.add(4);
-        list1.add(3);
-        list2.add(5);
-        list2.add(6);
-        list2.add(4);
-        System.out.println( solution2.addTwoNumbers(list1,list2));
+        ListNode list1 = new ListNode(5);
+        list1.next = new ListNode(4);
+        list1.next.next = new ListNode(3);
+        ListNode list2 = new ListNode(5);
+        list2.next = new ListNode(6);
+        list2.next.next = new ListNode(4);
+        ListNode list3 = solution2.addTwoNumbers1(list1, list2);
+        while (list3 != null) {
+            System.out.print(list3.val + "->");
+            list3 = list3.next;
+        }
     }
 }
--- LeetCodeJava/leetcode/src/com/leetcode/Power.java
+++ LeetCodeJava/leetcode/src/com/leetcode/Power.java
@@ -4,18 +4,29 @@ package com.leetcode;
  * Created by qiudeyang on 20/12/16.
  */
 class Solution50 {
+    //用全局变量来表征输入的合法性
+    boolean validInput = true;
     public double myPow(double x, int n) {
-        boolean validInput = true;
         if (isEqual(x,0.0) && n < 0){
             validInput = false;
             return 0.0;
         }
-        int unsignedN = Math.abs(n);
-        double result = powerUnsigned(x,unsignedN);
-        if (n < 0){
-            result = 1 / result;
+        if (n == 0){
+            return 1.0;
         }
-        return result;
+        if (n == Integer.MIN_VALUE){
+            x = x * x;
+            n = n >> 1;
+        }
+        if (n < 0){
+            n = -n;
+            x = 1/x;
+        }
+        if ((n & 1) == 1){
+            return x*myPow(x*x,n>>1);
+        }else{
+            return myPow(x*x,n>>1);
+        }
     }
     public boolean isEqual(double num1,double num2){
         if (Math.abs(num1-num2)<0.000001){
@@ -24,25 +35,11 @@ class Solution50 {
             return false;
         }
     }
-    public double powerUnsigned(double x,int unsignedN){
-        if (unsignedN == 0){
-            return 1.0;
-        }
-        if (unsignedN == 1){
-            return x;
-        }
-        double result = powerUnsigned(x,unsignedN>>1);//递归调用，用位运算比除2更快
-        result *= result;
-        if ((unsignedN & 1) == 1){   //位运算判断奇数更快
-            result *= x;
-        }
-        return result;
-    }
 }
 
 public class Power {
     public static void main(String[] args) {
         Solution50 solution50 = new Solution50();
-        System.out.println(solution50.myPow(2, 3));
+        System.out.println(solution50.myPow(2.00000,-2147483648));
     }
 }
