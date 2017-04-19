06bd515f6d1e4c9a00c47639cc65b67fcba87430
qiudeyang <969387319@qq.com>
2017-04-19 20:05:23 +0800

    update

--- LeetCodeJava/leetcode/src/com/leetcode/Sum3.java
+++ LeetCodeJava/leetcode/src/com/leetcode/Sum3.java
@@ -1,39 +1,52 @@
 package com.leetcode;
 
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
 /**
  * Created by qiudeyang on 07/06/16.
  */
-class Solution15{
-    public List<List<Integer>> threeSum(int[] nums){
-        List<Integer>  list = new LinkedList<Integer>();
-        List<List<Integer>>  list1 = new LinkedList<List<Integer>>();
-        for(int i = 0;i < nums.length;i++){
-            for(int j = i+1;j < nums.length;j++){
-                for (int k = j+1 ; k < nums.length;k++){
-                    if(nums[i] + nums[j] + nums[k] == 0){
-                        list.add(nums[i]);
-                        list.add(nums[j]);
-                        list.add(nums[k]);
-                        list1.add(list);
-                        for(int m = 0;m < list.size();m++){
-                            System.out.println(list.get(m));
-                        }
+class Solution15 {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new LinkedList<>();
+        if (nums == null || nums.length < 3) {
+            return result;
+        }
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int lo = i + 1;
+            int hi = nums.length - 1;
+            int sum = 0 - nums[i];
+            while (lo < hi) {
+                if (nums[lo] + nums[hi] == sum) {
+                    result.add(Arrays.asList(nums[i], nums[lo], nums[hi]));
+                    while (lo < hi && nums[lo] == nums[lo + 1]) {
+                        lo++;
                     }
+                    while (lo < hi && nums[hi] == nums[hi - 1]) {
+                        hi--;
+                    }
+                    lo++;
+                    hi--;
+                } else if (nums[lo] + nums[hi] < sum) {
+                    lo++;
+                } else {
+                    hi--;
                 }
-
             }
         }
-        list.clear();
-        return list1;
+        return result;
     }
 }
+
 public class Sum3 {
-    public static void main(String[] args){
-        Solution15  solution = new Solution15();
+    public static void main(String[] args) {
+        Solution15 solution15 = new Solution15();
         int[] nums = {-1, 0, 1, 2, -1, -4};
-        solution.threeSum(nums);
+        System.out.println(solution15.threeSum(nums));
     }
 }
