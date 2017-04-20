dcca9d0ed6fa58fc0ec2d57d872da0f50e7b3a3a
qiudeyang <969387319@qq.com>
2017-04-20 16:27:21 +0800

    update

--- LeetCodeJava/leetcode/src/com/leetcode/UniqueBinarySearchTrees.java
+++ LeetCodeJava/leetcode/src/com/leetcode/UniqueBinarySearchTrees.java
@@ -3,24 +3,25 @@ package com.leetcode;
 /**
  * Created by qiudeyang on 08/11/16.
  */
+//卡特兰数。
 class Solution96 {
     public int numTrees(int n) {
-        int[] G = new int[n + 1];
-        G[0] = 1;
-        G[1] = 1;
-        for (int i = 2; i <= n; i++) {
-            for (int j = 1; j <= i; j++) {
-                G[i] += G[j - 1] * G[i - j];
+        int[] dp = new int[n+1];
+        dp[0]=1;//空节点
+        dp[1]=1;
+        for (int i = 2; i <= n ; i++) {
+            for (int j = 0; j < i; j++) {
+                dp[i] += dp[j]*dp[i-j-1];
             }
         }
-        return G[n];
+        return dp[n];
     }
 }
 
 public class UniqueBinarySearchTrees {
     public static void main(String[] args) {
         Solution96 solution96 = new Solution96();
-        int n = 3;
+        int n = 6;
         System.out.println(solution96.numTrees(n));
     }
 }
