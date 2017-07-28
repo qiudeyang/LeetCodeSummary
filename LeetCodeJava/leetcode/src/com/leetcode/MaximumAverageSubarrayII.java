04ea864efc9e6b7e38636259f44205f859f6880a
qiudeyang <qiudeyang@bytedance.com>
2017-07-28 11:31:48 +0800

    update

---
+++ LeetCodeJava/leetcode/src/com/leetcode/MaximumAverageSubarrayII.java
@@ -0,0 +1,18 @@
+package com.leetcode;
+
+/**
+ * Created by qiudeyang on 2017/7/20.
+ */
+class Solution644 {
+    public double findMaxAverage(int[] nums, int k) {
+       return 1;
+    }
+}
+
+public class MaximumAverageSubarrayII {
+    public static void main(String[] args) {
+        Solution644 solution644 = new Solution644();
+        int[] nums = {1, 12, -5, -6, 50, 3};
+        System.out.println(solution644.findMaxAverage(nums, 4));
+    }
+}
